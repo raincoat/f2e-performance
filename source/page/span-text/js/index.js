68c6fc2af20cf7d51745dfad6758dc216fe7b395
yangqing <yangqing@douban.com>
2013-04-17 13:34:31 +0800

    添加span-text

---
+++ source/page/span-text/js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+  console.time('splite-text-to-span')
+  console.profile('splite-text-to-span')
+  var pureText = $('.pure-text')
+    , str = $.trim(pureText.html())
+    , wrap = $('<div>')
+    , i = 0
+    , l = str.length;
+
+  while (i < l) {
+    wrap.append($('<span>', {
+      text: str.charAt(i)
+    }))
+    i++
+  }
+
+  var spanText = $('<p>', {
+    'class': 'span-text'
+  })
+  spanText.html(wrap.html())
+  pureText.after(spanText)
+  console.timeEnd('splite-text-to-span')
+  console.profileEnd('splite-text-to-span')
+})
+
+$(function(){
+  var noteWord = $('.word-note')
+  var note = $('<div>', {
+    'class': 'note'
+  })
+  $('body').append(note)
+  var offset = noteWord.position()
+  offset.top -= 5
+  offset.left += 3
+  note.css(offset)
+})
